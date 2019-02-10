300040624f475b79c57dbf6cab18b8e733926dac
yusuffatal <31797247+yusuffatal@users.noreply.github.com>
2019-02-10 19:42:29 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_2235422396674875";
+$raw_uid="baaa70d5-da21-4e2f-8636-55c97e59c76b";
+$user_name="Muhammad+Yusuf";
+$invite_code="FAElrcabwx";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="c7fbd734ec8be7b7";
+$device_type="OPPO++CPH1803";
+
+
+?>
